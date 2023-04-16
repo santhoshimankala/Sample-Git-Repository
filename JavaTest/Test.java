328c3ac89442a52b77cfe905ec7060e9befb2303
santhoshimankala <105377260+santhoshimankala@users.noreply.github.com>
2023-04-16 13:54:06 +0530

    Update Test.java

--- JavaTest/Test.java
+++ JavaTest/Test.java
@@ -3,6 +3,7 @@ package JavaTest;
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is the first commit and push");
+        System.out.println("merge")
     }
 }
 
