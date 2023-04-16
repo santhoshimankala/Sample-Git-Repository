0638dfa4281673f056b04f89fe0af29bb88a9998
Mankala Santhoshi <santhoshi.mankala@modak.com>
2023-04-16 13:52:02 +0530

    added new file.

---
+++ JavaTest/Test.java
@@ -0,0 +1,8 @@
+package JavaTest;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("This is the first commit and push");
+    }
+}
+
--- Sampletest.java
+++
@@ -1,5 +0,0 @@
-public class Sampletest {
-    public static void main(String[] args) {
-        System.out.println("Hi this is my first commit from intellij!!!!");
-    }
-}
