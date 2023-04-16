7a1203973eaeb90043f57f9c8cf362e67d6c60a8
Mankala Santhoshi <santhoshi.mankala@modak.com>
2023-04-16 14:15:56 +0530

    added new file.

--- JavaTest/Test.java
+++ JavaTest/Test.java
@@ -4,7 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("This is the first commit and push");
         System.out.println("merge");
-        System.out.println("deleting last commit not pushed");
     }
 }
 
